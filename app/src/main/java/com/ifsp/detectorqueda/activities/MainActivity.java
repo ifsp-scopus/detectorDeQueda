7de9a05ae0d3ff8976c3e53e43c8ec317559e430
Denis Magno <denis_magno16@hotmail.com>
2018-05-04 17:15:24 -0300

    Criação do projeto, da estrutura dos pacotes e configuração do nome do projeto no AndroidStudio #1

---
+++ app/src/main/java/com/ifsp/detectorqueda/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ifsp.detectorqueda.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ifsp.detectorqueda.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
