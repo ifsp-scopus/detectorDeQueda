97adf0172bc6581c6522d55d68bdbb103c3b9258
Denis Magno <denis_magno16@hotmail.com>
2018-05-04 17:25:41 -0300

    Criação do botão de logar na MainActivity, Criação da Activity DetectorActivity, configuração da hierarquia dessas duas telas no arquivo de configuração AndroidManifest #2

--- app/src/main/java/com/ifsp/detectorqueda/activities/MainActivity.java
+++ app/src/main/java/com/ifsp/detectorqueda/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ifsp.detectorqueda.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ifsp.detectorqueda.R;
 
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     *  Inicia 'DetectorActivity'
+     *
+     * @param view  Objeto que representa o botão que foi clicado.
+     * @author      Denis Magno.
+     */
+    public void bLogar(View view){
+        Intent in = new Intent(this, DetectorActivity.class);
+        this.startActivity(in);
     }
 }
